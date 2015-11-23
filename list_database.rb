b8e6c76bdbd7a849131f5ea97c6ddb666159fdd3
alexander <alexander@alexander-desk.hsd1.pa.comcast.net>
2015-11-23 15:57:42 -0500

    added way to delete items from database

--- list_database.rb
+++ list_database.rb
@@ -9,3 +9,15 @@ Ask.all.each do |ask|
 	puts "#{ask.id}: #{ask.text}".blue if ask.id % 2 == 0
 	puts "#{ask.id}: #{ask.text}".yellow if ask.id % 2 == 1
 end
+
+puts "Enter id of item to delete: ".chomp.red
+
+var = gets.chomp
+
+ask = Ask.find(var.to_i)
+
+puts "Are you sure you want to delete this ask? (press any key to continue) \n#{ask.text}"
+
+gets
+
+ask.destroy
